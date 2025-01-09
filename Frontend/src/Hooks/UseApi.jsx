eb4b81e67556d208c9a2cf4fe202f2ea5a457033
Samuel Pouard <thosam73000@gmail.com>
2025-01-10 00:19:14 +0100

    argentBank2

--- Frontend/src/Hooks/UseApi.jsx
+++ Frontend/src/Hooks/UseApi.jsx
@@ -1,55 +1,3 @@
-// import { useState, useEffect } from "react";
-
-// const useApi = (url, token, method, body = null) => {
-//   const [data, setData] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       if (!token) {
-//         setError("Token is missing");
-//         setLoading(false);
-//         return;
-//       }
-//       try {
-//         const response = await fetch(url, {
-//           method,
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//           },
-//           ...(body && { body: JSON.stringify(body) }),
-//         });
-
-//         if (!response.ok) {
-//           throw new Error(`Failed to fetch data: ${response.statusText}`);
-//         }
-
-//         const result = await response.json();
-//         setData(result.body);
-//       } catch (error) {
-//         setError(error.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, [url, token, method, body]);
-
-//   return { data, loading, error };
-// };
-
-// export default useApi;
-
-
-
-
-
-
-
-
 import { useState, useEffect } from "react";
 
 const useApi = (url, token, method, body = null) => {
@@ -70,6 +18,7 @@ const useApi = (url, token, method, body = null) => {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
+            // authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NDg2YzdkZjJmNTI2MDNkY2UxNGM5OSIsImlhdCI6MTczNjQ2MjkxNSwiZXhwIjoxNzM2NTQ5MzE1fQ.xLJT2lSs5aTqsbRxV_LJQISWqNPGT7Uxt_ahdJ3-AVE",
           },
           ...(body && { body: JSON.stringify(body) }),
         });
@@ -94,3 +43,10 @@ const useApi = (url, token, method, body = null) => {
 };
 
 export default useApi;
+
+
+
+
+
+
+
--- Frontend/src/Pages/User/User.jsx
+++ Frontend/src/Pages/User/User.jsx
@@ -1,184 +1,3 @@
-// import { useEffect, useState } from "react";
-// import "./User.css";
-// import TransactionUser from "../../Components/TransactionUser/TransactionUser";
-// import { useDispatch } from "react-redux";
-// import { setUserName } from "../../Redux/Features/UserSlice";
-// import { useSelector } from "react-redux";
-// import useApi from "../../Hooks/UseApi";
-
-// export default function User() {
-//   const hasToken = localStorage.getItem("token"); // Récupérer le token dans le localStorage
-
-//   const [editName, setEditName] = useState(false);
-//   const dispatch = useDispatch();
-
-  
-//   //  on recupere la valeur du userName daans le store pour l'afficher dans l'input
-//   const userName = useSelector((state) => state.user.userName);
-//   //  on recupere la valeur de l'input userName pour la mettre à jour dans le store
-//   const [userNamePut, setUserNamePut] = useState(userName || ""); 
-
-
-
-//   //////// Utilise le hook personnalisé pour l'appel API
-//   const { data, loading, error , method } = useApi(
-//     "http://localhost:3001/api/v1/user/profile",
-//     hasToken,
-//     "GET"
-//   );
-
-//   useEffect(() => {
-//     if (data) {
-//       dispatch(setUserName(data.userName)); // Mettre à jour le store Redux avec le nom d'utilisateur
-//     }
-//   }, [data, dispatch]);
-
-//   if (loading) return <div>Chargement...</div>;
-//   if (error) return <div>Erreur: {error}</div>;
-
-//   console.log(data);
-
-
-
-//   /////////////// call API changement de UserName
-
-//   const handleInputChange = (e) => {
-//     setUserNamePut(e.target.value);
-//   };
-
-//   const fetchPutUser = async () => {
-//     if (!hasToken) {
-//       console.error("Token manquant !");
-//       return;
-//     }
-
-//     try {
-//       const response = await fetch(
-//         "http://localhost:3001/api/v1/user/profile",
-//         {
-//           method: "PUT",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${hasToken}`,
-//           },
-//           body: JSON.stringify({ userName: userNamePut }),
-//         }
-//       );
-
-//       if (response.ok) {
-//         const result = await response.json();
-//         console.log("Mise à jour réussie :", result);
-
-//         setEditName(false); // Fermer l'édition
-//         dispatch(setUserName(userNamePut)); // Mettre à jour le store Redux
-//       } else {
-//         console.error(
-//           "Erreur dans la réponse :",
-//           response.status,
-//           await response.text()
-//         );
-//       }
-//     } catch (error) {
-//       console.error("Erreur lors de la requête :", error);
-//     }
-//   };
-
-//   if (loading) return <p>Chargement...</p>;
-//   if (error) return <p>Erreur : {error}</p>;
-
- 
-
-//   return (
-//     <section className='section-user'>
-//       {editName ? (
-//         <div className='container-title-input-btn-user'>
-//           <h1 className='title-user'>
-//             Welcome back <br />
-//           </h1>
-//           <div className='container-input-user'>
-//             <input
-//               type='text'
-//               className='input-edit-user'
-//               placeholder={userName}
-//               value={userNamePut} // Valeur de l'input liée à l'état
-//               onChange={handleInputChange} // Mettre à jour l'état à chaque changement
-//             />
-//             <input
-//               type='text'
-//               className='input-edit-user'
-//               placeholder={data.firstName}
-//               disabled
-//             />
-//             <input
-//               type='text'
-//               className='input-edit-user'
-//               placeholder={data.lastName}
-//               disabled
-//             />
-//           </div>
-//           <div className='container-btn-user'>
-//             <button
-//               className='btn-edit-user'
-//               type='submit'
-//               onClick={fetchPutUser}>
-//               Save
-//             </button>
-//             <button
-//               className='btn-edit-user'
-//               onClick={() => setEditName(!editName)}>
-//               Cancel
-//             </button>
-//           </div>
-//         </div>
-//       ) : (
-//         <div>
-//           <h1 className='title-user'>
-//             Welcome back <br />
-//             <span>{data?.firstName}</span> <span>{data?.lastName}</span>!
-//           </h1>
-//           <button
-//             className='btn-edit-user'
-//             onClick={() => setEditName(!editName)}>
-//             Edit Name
-//           </button>
-//         </div>
-//       )}
-
-//       <div className='container-transaction-user'>
-//         <TransactionUser
-//           texte1='Argent Bank Checking (x8349)'
-//           texte2='Available Balance'
-//           title='$2,082.79'
-//         />
-//         <TransactionUser
-//           texte1='Argent Bank Savings (x6712)'
-//           texte2='Available Balance'
-//           title='$10,928.42'
-//         />
-//         <TransactionUser
-//           texte1='Argent Bank Credit Card (x8349)'
-//           texte2='Current Balance'
-//           title='$184.30'
-//         />
-//       </div>
-//     </section>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { useEffect, useState } from "react";
 import "./User.css";
 import TransactionUser from "../../Components/TransactionUser/TransactionUser";
@@ -191,11 +10,11 @@ export default function User() {
   const [editName, setEditName] = useState(false);
   const dispatch = useDispatch();
 
-  const userName = useSelector((state) => state.user.userName);
+  const userName = useSelector((state) => state.user.userName);// recupere le nom de l'utilisateur dans le store
   const [userNamePut, setUserNamePut] = useState(userName || "");
 
   //////// Hook pour récupérer les données utilisateur (GET)
-  const { data: userData, loading, error } = useApi(
+  const { data: userData, loading } = useApi(
     "http://localhost:3001/api/v1/user/profile",
     token,
     "GET"
@@ -206,10 +25,11 @@ export default function User() {
       dispatch(setUserName(userData.userName));
     }
   }, [userData, dispatch]);
+  console.log(token)
 
   //////// Hook pour mettre à jour les données utilisateur (PUT)
   const [putData, setPutData] = useState(null);
-  const { data: updateResponse, loading: loadingPut, error: errorPut } = useApi(
+  const { data: updateResponse, loading: loadingPut } = useApi(
     putData?.url,
     putData?.token,
     "PUT",
@@ -243,8 +63,7 @@ export default function User() {
   }, [updateResponse, dispatch, userNamePut]);
 
   if (loading || loadingPut) return <p>Chargement...</p>;
-  if (error || errorPut) return <p>Erreur : {error || errorPut}</p>;
-
+  if (!userData) return <p>Aucune donnée disponible.</p>;
   return (
     <section className="section-user">
       {editName ? (
@@ -324,3 +143,25 @@ export default function User() {
     </section>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
