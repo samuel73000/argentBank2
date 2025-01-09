4dbda2455aec246ec5ae74d4a5a88dad9faf625d
Samuel Pouard <thosam73000@gmail.com>
2025-01-09 23:53:49 +0100

    argentbank2

--- Frontend/src/Hooks/UseApi.jsx
+++ Frontend/src/Hooks/UseApi.jsx
@@ -1,6 +1,58 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from "react";
 
-const useApi = (url, token) => {
+// const useApi = (url, token, method, body = null) => {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       if (!token) {
+//         setError("Token is missing");
+//         setLoading(false);
+//         return;
+//       }
+//       try {
+//         const response = await fetch(url, {
+//           method,
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//           ...(body && { body: JSON.stringify(body) }),
+//         });
+
+//         if (!response.ok) {
+//           throw new Error(`Failed to fetch data: ${response.statusText}`);
+//         }
+
+//         const result = await response.json();
+//         setData(result.body);
+//       } catch (error) {
+//         setError(error.message);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [url, token, method, body]);
+
+//   return { data, loading, error };
+// };
+
+// export default useApi;
+
+
+
+
+
+
+
+
+import { useState, useEffect } from "react";
+
+const useApi = (url, token, method, body = null) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -14,15 +66,16 @@ const useApi = (url, token) => {
       }
       try {
         const response = await fetch(url, {
-          method: "GET",
+          method,
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
+          ...(body && { body: JSON.stringify(body) }),
         });
 
         if (!response.ok) {
-          throw new Error("Failed to fetch data");
+          throw new Error(`Failed to fetch data: ${response.statusText}`);
         }
 
         const result = await response.json();
@@ -35,7 +88,7 @@ const useApi = (url, token) => {
     };
 
     fetchData();
-  }, [url, token]);
+  }, [url, token, method, body]);
 
   return { data, loading, error };
 };
--- Frontend/src/Pages/User/User.jsx
+++ Frontend/src/Pages/User/User.jsx
@@ -1,162 +1,324 @@
+// import { useEffect, useState } from "react";
+// import "./User.css";
+// import TransactionUser from "../../Components/TransactionUser/TransactionUser";
+// import { useDispatch } from "react-redux";
+// import { setUserName } from "../../Redux/Features/UserSlice";
+// import { useSelector } from "react-redux";
+// import useApi from "../../Hooks/UseApi";
+
+// export default function User() {
+//   const hasToken = localStorage.getItem("token"); // Récupérer le token dans le localStorage
+
+//   const [editName, setEditName] = useState(false);
+//   const dispatch = useDispatch();
+
+  
+//   //  on recupere la valeur du userName daans le store pour l'afficher dans l'input
+//   const userName = useSelector((state) => state.user.userName);
+//   //  on recupere la valeur de l'input userName pour la mettre à jour dans le store
+//   const [userNamePut, setUserNamePut] = useState(userName || ""); 
+
+
+
+//   //////// Utilise le hook personnalisé pour l'appel API
+//   const { data, loading, error , method } = useApi(
+//     "http://localhost:3001/api/v1/user/profile",
+//     hasToken,
+//     "GET"
+//   );
+
+//   useEffect(() => {
+//     if (data) {
+//       dispatch(setUserName(data.userName)); // Mettre à jour le store Redux avec le nom d'utilisateur
+//     }
+//   }, [data, dispatch]);
+
+//   if (loading) return <div>Chargement...</div>;
+//   if (error) return <div>Erreur: {error}</div>;
+
+//   console.log(data);
+
+
+
+//   /////////////// call API changement de UserName
+
+//   const handleInputChange = (e) => {
+//     setUserNamePut(e.target.value);
+//   };
+
+//   const fetchPutUser = async () => {
+//     if (!hasToken) {
+//       console.error("Token manquant !");
+//       return;
+//     }
+
+//     try {
+//       const response = await fetch(
+//         "http://localhost:3001/api/v1/user/profile",
+//         {
+//           method: "PUT",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${hasToken}`,
+//           },
+//           body: JSON.stringify({ userName: userNamePut }),
+//         }
+//       );
+
+//       if (response.ok) {
+//         const result = await response.json();
+//         console.log("Mise à jour réussie :", result);
+
+//         setEditName(false); // Fermer l'édition
+//         dispatch(setUserName(userNamePut)); // Mettre à jour le store Redux
+//       } else {
+//         console.error(
+//           "Erreur dans la réponse :",
+//           response.status,
+//           await response.text()
+//         );
+//       }
+//     } catch (error) {
+//       console.error("Erreur lors de la requête :", error);
+//     }
+//   };
+
+//   if (loading) return <p>Chargement...</p>;
+//   if (error) return <p>Erreur : {error}</p>;
+
+ 
+
+//   return (
+//     <section className='section-user'>
+//       {editName ? (
+//         <div className='container-title-input-btn-user'>
+//           <h1 className='title-user'>
+//             Welcome back <br />
+//           </h1>
+//           <div className='container-input-user'>
+//             <input
+//               type='text'
+//               className='input-edit-user'
+//               placeholder={userName}
+//               value={userNamePut} // Valeur de l'input liée à l'état
+//               onChange={handleInputChange} // Mettre à jour l'état à chaque changement
+//             />
+//             <input
+//               type='text'
+//               className='input-edit-user'
+//               placeholder={data.firstName}
+//               disabled
+//             />
+//             <input
+//               type='text'
+//               className='input-edit-user'
+//               placeholder={data.lastName}
+//               disabled
+//             />
+//           </div>
+//           <div className='container-btn-user'>
+//             <button
+//               className='btn-edit-user'
+//               type='submit'
+//               onClick={fetchPutUser}>
+//               Save
+//             </button>
+//             <button
+//               className='btn-edit-user'
+//               onClick={() => setEditName(!editName)}>
+//               Cancel
+//             </button>
+//           </div>
+//         </div>
+//       ) : (
+//         <div>
+//           <h1 className='title-user'>
+//             Welcome back <br />
+//             <span>{data?.firstName}</span> <span>{data?.lastName}</span>!
+//           </h1>
+//           <button
+//             className='btn-edit-user'
+//             onClick={() => setEditName(!editName)}>
+//             Edit Name
+//           </button>
+//         </div>
+//       )}
+
+//       <div className='container-transaction-user'>
+//         <TransactionUser
+//           texte1='Argent Bank Checking (x8349)'
+//           texte2='Available Balance'
+//           title='$2,082.79'
+//         />
+//         <TransactionUser
+//           texte1='Argent Bank Savings (x6712)'
+//           texte2='Available Balance'
+//           title='$10,928.42'
+//         />
+//         <TransactionUser
+//           texte1='Argent Bank Credit Card (x8349)'
+//           texte2='Current Balance'
+//           title='$184.30'
+//         />
+//       </div>
+//     </section>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { useEffect, useState } from "react";
 import "./User.css";
 import TransactionUser from "../../Components/TransactionUser/TransactionUser";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUserName } from "../../Redux/Features/UserSlice";
-import { useSelector } from "react-redux";
 import useApi from "../../Hooks/UseApi";
 
 export default function User() {
-  const hasToken = localStorage.getItem("token"); // Récupérer le token dans le localStorage
-
+  const token = localStorage.getItem("token"); // Récupérer le token dans le localStorage
   const [editName, setEditName] = useState(false);
   const dispatch = useDispatch();
 
-  
-  //  on recupere la valeur du userName daans le store pour l'afficher dans l'input
   const userName = useSelector((state) => state.user.userName);
-  //  on recupere la valeur de l'input userName pour la mettre à jour dans le store
-  const [userNamePut, setUserNamePut] = useState(userName || ""); 
+  const [userNamePut, setUserNamePut] = useState(userName || "");
 
-
-
-  //////// Utilise le hook personnalisé pour l'appel API
-  const { data, loading, error } = useApi(
+  //////// Hook pour récupérer les données utilisateur (GET)
+  const { data: userData, loading, error } = useApi(
     "http://localhost:3001/api/v1/user/profile",
-    hasToken
+    token,
+    "GET"
   );
 
   useEffect(() => {
-    if (data) {
-      dispatch(setUserName(data.userName)); // Mettre à jour le store Redux avec le nom d'utilisateur
+    if (userData) {
+      dispatch(setUserName(userData.userName));
     }
-  }, [data, dispatch]);
+  }, [userData, dispatch]);
 
-  if (loading) return <div>Chargement...</div>;
-  if (error) return <div>Erreur: {error}</div>;
-
-  console.log(data);
-
-
-
-  /////////////// call API changement de UserName
+  //////// Hook pour mettre à jour les données utilisateur (PUT)
+  const [putData, setPutData] = useState(null);
+  const { data: updateResponse, loading: loadingPut, error: errorPut } = useApi(
+    putData?.url,
+    putData?.token,
+    "PUT",
+    putData?.body
+  );
 
   const handleInputChange = (e) => {
     setUserNamePut(e.target.value);
   };
 
-  const fetchPutUser = async () => {
-    if (!hasToken) {
+  const handleSave = () => {
+    if (!token) {
       console.error("Token manquant !");
       return;
     }
 
-    try {
-      const response = await fetch(
-        "http://localhost:3001/api/v1/user/profile",
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${hasToken}`,
-          },
-          body: JSON.stringify({ userName: userNamePut }),
-        }
-      );
+    setPutData({
+      url: "http://localhost:3001/api/v1/user/profile",
+      token: token,
+      body: { userName: userNamePut },
+    });
 
-      if (response.ok) {
-        const result = await response.json();
-        console.log("Mise à jour réussie :", result);
-
-        setEditName(false); // Fermer l'édition
-        dispatch(setUserName(userNamePut)); // Mettre à jour le store Redux
-      } else {
-        console.error(
-          "Erreur dans la réponse :",
-          response.status,
-          await response.text()
-        );
-      }
-    } catch (error) {
-      console.error("Erreur lors de la requête :", error);
-    }
+    setEditName(false);
   };
 
-  if (loading) return <p>Chargement...</p>;
-  if (error) return <p>Erreur : {error}</p>;
+  useEffect(() => {
+    if (updateResponse) {
+      console.log("Mise à jour réussie :", updateResponse);
+      dispatch(setUserName(userNamePut));
+    }
+  }, [updateResponse, dispatch, userNamePut]);
 
+  if (loading || loadingPut) return <p>Chargement...</p>;
+  if (error || errorPut) return <p>Erreur : {error || errorPut}</p>;
 
   return (
-    <section className='section-user'>
+    <section className="section-user">
       {editName ? (
-        <div className='container-title-input-btn-user'>
-          <h1 className='title-user'>
+        <div className="container-title-input-btn-user">
+          <h1 className="title-user">
             Welcome back <br />
           </h1>
-          <div className='container-input-user'>
+          <div className="container-input-user">
             <input
-              type='text'
-              className='input-edit-user'
+              type="text"
+              className="input-edit-user"
               placeholder={userName}
-              value={userNamePut} // Valeur de l'input liée à l'état
-              onChange={handleInputChange} // Mettre à jour l'état à chaque changement
+              value={userNamePut}
+              onChange={handleInputChange}
             />
             <input
-              type='text'
-              className='input-edit-user'
-              placeholder={data.firstName}
+              type="text"
+              className="input-edit-user"
+              placeholder={userData?.firstName}
               disabled
             />
             <input
-              type='text'
-              className='input-edit-user'
-              placeholder={data.lastName}
+              type="text"
+              className="input-edit-user"
+              placeholder={userData?.lastName}
               disabled
             />
           </div>
-          <div className='container-btn-user'>
+          <div className="container-btn-user">
             <button
-              className='btn-edit-user'
-              type='submit'
-              onClick={fetchPutUser}>
+              className="btn-edit-user"
+              type="submit"
+              onClick={handleSave}
+            >
               Save
             </button>
             <button
-              className='btn-edit-user'
-              onClick={() => setEditName(!editName)}>
+              className="btn-edit-user"
+              onClick={() => setEditName(!editName)}
+            >
               Cancel
             </button>
           </div>
         </div>
       ) : (
         <div>
-          <h1 className='title-user'>
+          <h1 className="title-user">
             Welcome back <br />
-            <span>{data?.firstName}</span> <span>{data?.lastName}</span>!
+            <span>{userData?.firstName}</span> <span>{userData?.lastName}</span>!
           </h1>
           <button
-            className='btn-edit-user'
-            onClick={() => setEditName(!editName)}>
+            className="btn-edit-user"
+            onClick={() => setEditName(!editName)}
+          >
             Edit Name
           </button>
         </div>
       )}
 
-      <div className='container-transaction-user'>
+      <div className="container-transaction-user">
         <TransactionUser
-          texte1='Argent Bank Checking (x8349)'
-          texte2='Available Balance'
-          title='$2,082.79'
+          texte1="Argent Bank Checking (x8349)"
+          texte2="Available Balance"
+          title="$2,082.79"
         />
         <TransactionUser
-          texte1='Argent Bank Savings (x6712)'
-          texte2='Available Balance'
-          title='$10,928.42'
+          texte1="Argent Bank Savings (x6712)"
+          texte2="Available Balance"
+          title="$10,928.42"
         />
         <TransactionUser
-          texte1='Argent Bank Credit Card (x8349)'
-          texte2='Current Balance'
-          title='$184.30'
+          texte1="Argent Bank Credit Card (x8349)"
+          texte2="Current Balance"
+          title="$184.30"
         />
       </div>
     </section>
